99e45ba8cdc6ea08d014c25e17103a8fb4419377
Indira <kuttuwaindira1997@gmail.com>
2023-08-24 16:38:01 +0530

    Login/Logout and VIP member access

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc: {
+        // clientId: 'HYmqebWKAqTjcvFPaRlDriTVIYsVqiWP',
+        clientId: '0oaayb5maqdVXaxaf5d7',
+        // issuer: 'https://dev-n26r6asre3ihpqa1.okta.com/oauth2/default',
+        issuer: 'https://dev-66155568.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
